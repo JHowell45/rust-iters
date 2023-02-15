03985fce3cfe7738c0ef8674ac8db2834a39d541
Jacob Howell <jhowell45@googlemail.com>
2023-02-15 01:59:11 +0000

    number of elements.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use linked_lists::single::simple::SimpleLinkedList;
 
 fn test_simple_single_linked_list() {
     let mut root: SimpleLinkedList<i32> = SimpleLinkedList::new();
-    for n in 0..10 {
+    for n in 0..10000 {
         root.push(n);
     }
     for x in root.iter() {
